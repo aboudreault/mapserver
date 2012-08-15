501d3ded875a5f9c0afa4eb54676f54bb62ab6c0
Alan Boudreault <aboudreault@mapgears.com>
2012-08-15 14:51:54 +0000

    Fix SWIG compilation about msOffsetCurve issue...

--- mapserver.h
+++ mapserver.h
@@ -2743,17 +2743,17 @@ struct rendererVTableObj {
 	int (*cleanup)(void *renderer_data);
 } ;
 
+shapeObj *msOffsetCurve(shapeObj *p, double offset);
+#if defined HAVE_GEOS_OFFSET_CURVE
+shapeObj *msGEOSOffsetCurve(shapeObj *p, double offset);
+#endif
+
 #endif /* SWIG */
 
 #define MS_IMAGE_RENDERER(im) ((im)->format->vtable)
 #define MS_RENDERER_CACHE(renderer) ((renderer)->renderer_data)
 #define MS_IMAGE_RENDERER_CACHE(im) MS_RENDERER_CACHE(MS_IMAGE_RENDERER((im)))
 #define MS_MAP_RENDERER(map) ((map)->outputformat->vtable)
-
-shapeObj *msOffsetCurve(shapeObj *p, double offset);
-#if defined HAVE_GEOS_OFFSET_CURVE
-shapeObj *msGEOSOffsetCurve(shapeObj *p, double offset);
-#endif
 
 #ifdef __cplusplus
 }
